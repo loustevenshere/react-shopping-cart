ae02c4a93784b7252bd9ee8f745f8b911ea6026d
Louis Stevens <loustevenshere@gmail.com>
2021-01-21 19:48:57 -0500

    Did ProductContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
@@ -7,26 +7,37 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import { ProductContext } from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([
+			 ...cart, item
+		])
 	};
 
+	console.log("this is products", products);
+	console.log("this is cart", cart);
 	return (
 		<div className="App">
+			<ProductContext.Provider value={products} addItem={addItem}>
+
 			<Navigation cart={cart} />
 
 			{/* Routes */}
 			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
+				<Products />
 			</Route>
 
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
